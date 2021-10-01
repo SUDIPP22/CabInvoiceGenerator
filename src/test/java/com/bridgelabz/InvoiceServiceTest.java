10a11fe119d49032915f6480fb15bdd6e1ec873e
SUDIPP22 <panja370@gmail.com>
2021-10-01 14:42:11 +0530

    Modified : To calculate the aggregate total fare for multiple rides

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -1,12 +1,19 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+    public static InvoiceGenerator invoiceGenerator = null;
+
+    @BeforeAll
+    static void beforeAll() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     void givenDistanceAndTimeShouldReturnTheTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -15,10 +22,18 @@ public class InvoiceServiceTest {
 
     @Test
     void givenLessDistanceAndTimeShouldReturnTheMinimumFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    void givenMultipleRidesShouldReturnTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare, 0.0);
     }
 }
