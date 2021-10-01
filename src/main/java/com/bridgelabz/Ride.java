5ac5e0c10151cd1e045a4b1d50d4cb91d44fe736
SUDIPP22 <panja370@gmail.com>
2021-10-01 14:40:39 +0530

    Added : To generate multiple rides

---
+++ src/main/java/com/bridgelabz/Ride.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To generate multiple rides
+ */
+public class Ride {
+    public final double distance;
+    public final int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
